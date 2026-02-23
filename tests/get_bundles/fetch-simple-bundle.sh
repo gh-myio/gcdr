1295b80e23419540c6a71b6c4c20847ec7593d47
Rodrigo Lago <rplago@gmail.com>
2026-02-23 18:42:14 -0300

    update testes

--- tests/get_bundles/fetch-simple-bundle.sh
+++ tests/get_bundles/fetch-simple-bundle.sh
@@ -6,12 +6,14 @@
 # Production: https://gcdr-server.apps.myio-bas.com
 #API_URL="http://localhost:3015"
 API_URL="https://gcdr-api.a.myio-bas.com"
-CUSTOMER_ID="77777777-7777-7777-7777-777777777777"
+CUSTOMER_ID="84e0370e-636a-4741-9874-504b5e0b3577"
 # Fixed test API key (seeded in database)
-API_KEY="gcdr_cust_test_bundle_key_myio2026"
+#API_KEY="gcdr_cust_test_bundle_key_myio2026"
+API_KEY="gcdr_alarm_integration_key_2026"
 # X-Tenant-Id is optional - tenant is auto-discovered from API key
 # X-Central-Id is optional - filters devices by central ID
-CENTRAL_ID="9308af89-94b2-45e6-9e47-ae78f881afd2"
+CENTRAL_ID="e982edf9-edb1-4aa6-8a14-4782465ae5a3"
+  
 OUTPUT_FILE="$(dirname "$0")/simple_bundle_output.json"
 
 echo "Fetching simplified bundle..."
